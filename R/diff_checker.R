b95ad46b1e48e1be88862e8feff937e84cbb0f14
Oliver Keyes <ironholds@gmail.com>
2014-04-13 13:07:09 -0700

    update wiki_diff to allow for multiple requests - expand the documentation and error checking to factor in the logic around uncached diffs.

--- R/diff_checker.R
+++ R/diff_checker.R
@@ -1,13 +1,23 @@
 #Check the result of wiki_diff for problems. We can at least warn about them.
 diff_checker <- function(diff_content){
   
+  diff_names <- names(unlist(diff_content))
+  
   #Are there invalid revIDs?
-  if(!is.null(diff_content$query$badrevids)){
+  bad_revs <- sum(grepl(x = diff_names, pattern = "badrevids"))
+  if(bad_revs){
     
-    #If so, hold, strip the names and warn
-    bad_ids <- unlist(diff_content$query$badrevids)
-    names(bad_ids) <- NULL
-    warning("This request contained invalid revisionIDs:", bad_ids, call. = FALSE)
+    #If so, warn
+    warning("This request contained ",bad_revs," invalid revisionIDs", call. = FALSE)
+    
+  }
+  
+  #Are there uncached pages?
+  uncached <- sum(grepl(x = diff_names, pattern = "diff.notcached"))
+  if(uncached){
+    
+    #If so, warn
+    warning("This request contained ",uncached, "uncached revisions; these will not be returned", call. = FALSE)
     
   }
   
--- R/wiki_diff.R
+++ R/wiki_diff.R
@@ -1,6 +1,6 @@
 #Retrieves a 'diff' between two revisions to a page
 wiki_diff <- function(con,
-                      revID,
+                      revisions,
                       properties = c("ids","flags","timestamp","user","userid","size",
                                              "sha1","contentmodel","comment","parsedcomment",
                                              "tags","flagged"),
@@ -11,11 +11,12 @@ wiki_diff <- function(con,
   direction <- match.arg(direction)
   properties <- match.arg(properties, several.ok = TRUE)
   
-  #Save the desired properties
+  #Save the desired properties and revisions
   properties <- paste(properties, collapse = "|")
+  revisions <- paste(revisions, collapse = "|")
   
   #Construct the URL
-  diff_url <- paste(con$URL,"&action=query&prop=revisions&rvprop=",properties,"&rvdiffto=",direction,"&rvcontentformat=text%2Fcss&revids=",revID, sep = "")
+  diff_url <- paste(con$URL,"&action=query&prop=revisions&rvprop=",properties,"&rvdiffto=",direction,"&rvcontentformat=text%2Fcss&revids=",revisions, sep = "")
   
   #Retrieve the content
   diff_content <- wiki_call(URL = diff_url)
