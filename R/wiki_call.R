e142f2381c031810908fb4eb5f50c59b905a8f76
Oliver Keyes <ironholds@gmail.com>
2014-04-13 14:31:24 -0700

    add error handler for in-API errors

--- R/wiki_call.R
+++ R/wiki_call.R
@@ -7,7 +7,16 @@ wiki_call <- function(URL){
   #Check the validity of the response
   wiki_checker(response)
   
-  #Parse it and return
-  return(wiki_parse(response = response))
+  #Parse it
+  parsed_response <- wiki_parse(response = response)
   
+  #Is there an error at the API end?
+  if(!is.null(parsed_response$error)){
+    
+    stop("The API returned an error, code: ",parsed_text$error$code)
+    
+  }
+  
+  #Otherwise, return
+  return(parsed_response)
 }
