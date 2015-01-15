72320eb0504884780bfd70cc51d22eb436b9a987
ironholds <ironholds@gmail.com>
2015-01-15 17:02:05 -0500

    add metadata tests

--- tests/testthat/test_metadata_retrieval.R
+++ tests/testthat/test_metadata_retrieval.R
@@ -7,3 +7,19 @@ test_that("backlinks can be retrieved through page_backlinks", {
 test_that("backlinks from a specific namespace can be retrieved through page_backlinks", {
   expect_true({page_backlinks("en","wikipedia", page = "Aaron Halfaker", namespaces = 0);TRUE})
 })
+
+test_that("links can be retrieved through page_links", {
+  expect_true({page_links("en","wikipedia", page = "Aaron Halfaker");TRUE})
+})
+
+test_that("links from a specific namespace can be retrieved through page_links", {
+  expect_true({page_links("en","wikipedia", page = "Aaron Halfaker", namespaces = 0);TRUE})
+})
+
+test_that("external links can be retrieved through page_links", {
+  expect_true({page_external_links("en","wikipedia", page = "Aaron Halfaker");TRUE})
+})
+
+test_that("external links with a particular protocol can be retrieved through page_links", {
+  expect_true({page_external_links("en","wikipedia", page = "Aaron Halfaker", protocol = "http");TRUE})
+})
